502bb5ae66d41439c26cbeb5fcd6efcfb3805c48
Egor Kuzminau <tribun395@gmail.com>
2024-06-11 19:15:31 +0200

    Removing the remainder of the files

--- out/webhelp-responsive/oxygen-webhelp/app/search/index/index-2.js
+++
@@ -1,2 +0,0 @@
-// Auto generated index for searching.
-var index2 = {"help":"0*20*2","sample":"1*10*-1$4,2*10*-1$4,3*10*-1$6,4*10*-1$6","sensitive":"0*20*1","text":"1*10*-1$5,2*10*-1$5,3*10*-1$7,4*10*-1$7"};
--- out/webhelp-responsive/oxygen-webhelp/app/search/index/index-3.js
+++
@@ -1,2 +0,0 @@
-// Auto generated index for searching.
-var index3 = {"topic":"1*40*-1$1$3$7,2*40*-1$1$3$7,3*40*-1$1$4$9,4*40*-1$1$4$9"};
